9739e0ad94d75cd18d1ac8399daa3abbd337c4f7
weston-wang <weston.y.wang@gmail.com>
2024-05-09 13:03:06 -0700

    remove unused ml stuff

--- TripleTale/ViewController.swift
+++ TripleTale/ViewController.swift
@@ -20,23 +20,6 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate, ARSKViewDel
     private var isFrozen = false
     private var firstSession = true
 
-    /// The ML model to be used for recognition of arbitrary objects.
-    private var _inceptionv3Model: Inceptionv3!
-    private var inceptionv3Model: Inceptionv3! {
-        get {
-            if let model = _inceptionv3Model { return model }
-            _inceptionv3Model = {
-                do {
-                    let configuration = MLModelConfiguration()
-                    return try Inceptionv3(configuration: configuration)
-                } catch {
-                    fatalError("Couldn't create Inceptionv3 due to: \(error)")
-                }
-            }()
-            return _inceptionv3Model
-        }
-    }
-    
     /// The ML model to be used for detection of arbitrary objects
     private var _yolo3Model: YOLOv3!
     private var yolo3Model: YOLOv3! {
@@ -302,28 +285,6 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate, ARSKViewDel
     
     // MARK: - Vision classification
     
-    // Vision classification request and model
-    /// - Tag: ClassificationRequest
-    private lazy var classificationRequest: VNCoreMLRequest = {
-        do {
-            // Instantiate the model from its generated Swift class.
-            let model = try VNCoreMLModel(for: inceptionv3Model.model)
-            let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
-                self?.processClassifications(for: request, error: error)
-            })
-            
-            // Crop input images to square area at center, matching the way the ML model was trained.
-            request.imageCropAndScaleOption = .centerCrop
-            
-            // Use CPU for Vision processing to ensure that there are adequate GPU resources for rendering.
-            request.usesCPUOnly = true
-            
-            return request
-        } catch {
-            fatalError("Failed to load Vision ML model: \(error)")
-        }
-    }()
-    
     /// - Tag: DetectionRequest
     private lazy var detectionRequest: VNCoreMLRequest = {
         do {
@@ -348,24 +309,6 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate, ARSKViewDel
     // Queue for dispatching vision classification requests
     private let visionQueue = DispatchQueue(label: "com.example.apple-samplecode.ARKitVision.serialVisionQueue")
     
-    // Run the Vision+ML classifier on the current image buffer.
-    /// - Tag: ClassifyCurrentImage
-    private func classifyCurrentImage() {
-        // Most computer vision tasks are not rotation agnostic so it is important to pass in the orientation of the image with respect to device.
-        let orientation = CGImagePropertyOrientation(UIDevice.current.orientation)
-        
-        let requestHandler = VNImageRequestHandler(cvPixelBuffer: currentBuffer!, orientation: orientation)
-        visionQueue.async {
-            do {
-                // Release the pixel buffer when done, allowing the next buffer to be processed.
-                defer { self.currentBuffer = nil }
-                try requestHandler.perform([self.classificationRequest])
-            } catch {
-                print("Error: Vision request failed with error \"\(error)\"")
-            }
-        }
-    }
-    
     private func detectCurrentImage() {
         // Most computer vision tasks are not rotation agnostic so it is important to pass in the orientation of the image with respect to device.
         let orientation = CGImagePropertyOrientation(UIDevice.current.orientation)
@@ -386,31 +329,6 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate, ARSKViewDel
     private var identifierString = ""
     private var confidence: VNConfidence = 0.0
     private var boundingBox: CGRect?
-    
-    // Handle completion of the Vision request and choose results to display.
-    /// - Tag: ProcessClassifications
-    func processClassifications(for request: VNRequest, error: Error?) {
-        guard let results = request.results else {
-            print("Unable to classify image.\n\(error!.localizedDescription)")
-            return
-        }
-        // The `results` will always be `VNClassificationObservation`s, as specified by the Core ML model in this project.
-        let classifications = results as! [VNClassificationObservation]
-        
-        // Show a label for the highest-confidence result (but only above a minimum confidence threshold).
-        if let bestResult = classifications.first(where: { result in result.confidence > 0.5 }),
-            let label = bestResult.identifier.split(separator: ",").first {
-            identifierString = String(label)
-            confidence = bestResult.confidence
-        } else {
-            identifierString = ""
-            confidence = 0
-        }
-        
-        DispatchQueue.main.async { [weak self] in
-            self?.displayClassifierResults()
-        }
-    }
     
     func processDetections(for request: VNRequest, error: Error?) {
         guard let results = request.results else {
