9cbba268fdde4a5f77b5b8334ad73c19c9b6885e
weston-wang <weston.y.wang@gmail.com>
2025-05-05 22:49:42 -0700

    fix mask bug

--- TripleTale/Support/Extensions.swift
+++ TripleTale/Support/Extensions.swift
@@ -56,44 +56,39 @@ extension UIImage {
 
         let inputCIImage = CIImage(cgImage: cgImage)
 
-        // Resize mask if needed
-        let resizedMask = mask.transformed(by: CGAffineTransform(scaleX: inputCIImage.extent.width / mask.extent.width,
-                                                                 y: inputCIImage.extent.height / mask.extent.height))
-
         guard let filter = CIFilter(name: "CIBlendWithMask") else { return nil }
         filter.setValue(inputCIImage, forKey: kCIInputImageKey)
-        filter.setValue(resizedMask, forKey: kCIInputMaskImageKey)
+        filter.setValue(mask, forKey: kCIInputMaskImageKey)
         filter.setValue(CIImage(color: .clear).cropped(to: inputCIImage.extent), forKey: kCIInputBackgroundImageKey)
 
         guard let outputImage = filter.outputImage else { return nil }
 
-        // Compute bounding box of mask content
-        let maskExtent = resizedMask.extent
-        let context = CIContext(options: [.useSoftwareRenderer: false])
-        guard let maskCG = context.createCGImage(resizedMask, from: maskExtent) else { return nil }
-
-        let width = Int(maskExtent.width)
-        let height = Int(maskExtent.height)
+        let width = Int(mask.extent.width)
+        let height = Int(mask.extent.height)
         let bytesPerRow = width
         let colorSpace = CGColorSpaceCreateDeviceGray()
 
-        guard let bitmapContext = CGContext(data: nil,
+        let context = CIContext()
+        guard let maskCG = context.createCGImage(mask, from: mask.extent),
+              let bitmapContext = CGContext(data: nil,
                                             width: width,
                                             height: height,
                                             bitsPerComponent: 8,
                                             bytesPerRow: bytesPerRow,
                                             space: colorSpace,
                                             bitmapInfo: CGImageAlphaInfo.none.rawValue),
-              let buffer = bitmapContext.data else { return nil }
+              let buffer = bitmapContext.data else {
+            return nil
+        }
 
         bitmapContext.draw(maskCG, in: CGRect(x: 0, y: 0, width: width, height: height))
 
-        // Find non-zero alpha bounding box
+        // Find bounding box of non-zero mask pixels
         var minX = width, minY = height, maxX = 0, maxY = 0
         for y in 0..<height {
             for x in 0..<width {
-                let pixelValue = buffer.load(fromByteOffset: y * bytesPerRow + x, as: UInt8.self)
-                if pixelValue > 10 {
+                let pixel = buffer.load(fromByteOffset: y * bytesPerRow + x, as: UInt8.self)
+                if pixel > 10 {
                     minX = min(minX, x)
                     maxX = max(maxX, x)
                     minY = min(minY, y)
@@ -104,11 +99,17 @@ extension UIImage {
 
         guard minX < maxX, minY < maxY else { return nil }
 
-        let cropRect = CGRect(x: minX, y: minY, width: maxX - minX, height: maxY - minY)
-            .applying(CGAffineTransform(scaleX: inputCIImage.extent.width / CGFloat(width),
-                                        y: inputCIImage.extent.height / CGFloat(height)))
+        // Flip y-axis because Core Image uses bottom-left origin
+        let flippedCropRect = CGRect(
+            x: minX,
+            y: height - maxY,
+            width: maxX - minX,
+            height: maxY - minY
+        ).integral
 
-        guard let croppedCG = context.createCGImage(outputImage.cropped(to: cropRect), from: cropRect) else { return nil }
+        guard let croppedCG = context.createCGImage(outputImage.cropped(to: flippedCropRect), from: flippedCropRect) else {
+            return nil
+        }
 
         return UIImage(cgImage: croppedCG, scale: self.scale, orientation: self.imageOrientation)
     }
